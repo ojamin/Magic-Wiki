e061792a309ca147c304bde2fa99453c3759d7ba
Benjamin Ohana <ben@ebotech.com>
2010-03-23 20:47:32 +0000

    fully working

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,44 +1,31 @@
 class WikiController < ApplicationController
+  
+  include MagicWiki
 
-  private
-  def wiki_sub(str)
-    switches = WikiItem::get_switches
-    switches.keys.each {|k|
-      if switches[k].class == String
-        str.gsub! k, switches[k]
-      elsif switches[k].class == Hash && (scn = str.scan(k).flatten).size > 0
-        if switches[k][:inline]
-          str.gsub! k, render_to_string(:inline => switches[k][:content].gsub('\1', scn[1]), :type => switches[k][:inline])
-        end
-      end
-    }
-    return str
-  end
-
-  public
   def view
-    render :text => "" and return unless params[:name] && params[:name].size > 0
-    @page = WikiItem.find_by_name(params[:name]) || WikiItem.new(:name => params[:name], :content => "")
+    magic_wiki_do_setup params[:name]
     if params[:commit] && params[:content]
-      @page.content = params[:content]
-      @page.save
+      begin
+        if @magic_wiki_editable
+          @page.content = params[:content]
+          @page.save
+        end
+      rescue
+      end
     end
-    @text = []
-    @text << render_to_string(:inline => "= link_to_remote :Edit, :url => {:controller => :wiki, :action => :edit, :id => '#{params[:name]}'}, :update =>:main_wiki_box", :type => :haml)
-    @text << "<br /><br />"
-    @text << "Empty page for #{params[:name]}" unless @page.content && @page.content.size > 0
-    @page.content.split("\n").each {|l|
-      @text << wiki_sub(l.gsub("\r", ""))
-    }
-    @text = @text.join("\n")
-    @text.html_safe!
-    render :partial => "view", :layout => true, :locals => {:page => @page, :text => @text}
+    render :text => magic_wiki_display and return if params[:commit]
+    render :text => magic_wiki_display, :layout => true and return
   end
 
   def edit
-    render :text => "" and return unless params[:name] && params[:name].size > 0
-    @page = WikiItem.find_by_name(params[:name]) || WikiItem.new(:name => params[:name], :content => "")
-    render :partial => "edit", :locals => {:page => @page}
+    begin
+      if @magic_wiki_editable
+        magic_wiki_do_setup params[:name]
+        render :partial => "wiki/edit", :locals => {:page => @page} and return
+      end
+    rescue
+    end
+    render :text => "" and return
   end
 
 end
--- lib/magic_wiki.rb
+++ lib/magic_wiki.rb
@@ -1,2 +1,43 @@
-# MagicWiki
-require "haml"
+# class ApplicationController <
+#class ActionController::Base
+module MagicWiki
+  require "haml"
+
+  def magic_wiki_sub(str)
+    switches = WikiItem::get_switches
+    switches.keys.each {|k|
+      if switches[k].class == String
+        str.gsub! k, switches[k]
+      elsif switches[k].class == Hash && (scn = str.scan(k).flatten).size > 0
+        if switches[k][:inline]
+          str.gsub! k, render_to_string(:inline => switches[k][:content].gsub('\1', scn[1]), :type => switches[k][:inline])
+        end
+      end
+    }
+    return str
+  end
+
+  def magic_wiki_do_setup(name)
+    render :text => "" and return unless name.size > 0
+    @page = WikiItem.find_by_name(name) || WikiItem.new(:name => name, :content => "")
+  end
+
+  def magic_wiki_display
+    render :text => "" and return unless @page
+    @text = []
+    begin
+      if @magic_wiki_editable
+        @text << render_to_string(:inline => "= link_to_remote :Edit, :url => {:controller => :wiki, :action => :edit, :id => '#{@page.name}'}, :update => :main_wiki_box", :type => :haml)
+        @text << "<br /><br />"
+      end
+    rescue
+    end
+    @text << "Empty page for #{@page.name}" unless @page.content && @page.content.size > 0
+    @page.content.split("\n").each {|l| @text << magic_wiki_sub(l.gsub("\r", "")) }
+    @text = @text.join("\n")
+    @text.html_safe!
+    outp = render_to_string(:partial => "wiki/view", :locals => {:page => @page, :text => @text}, :type => :haml)
+    return '<div id="main_wiki_box">' + outp + '</div>'
+  end
+
+end
