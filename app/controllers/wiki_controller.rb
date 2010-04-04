5d5c57c09ab687cf344b64cbed32f3b3a23c7f52
Benjamin Ohana <ben@ebotech.com>
2010-04-04 17:06:26 +0100

    fix

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -2,6 +2,12 @@ class WikiController < ApplicationController
   
   include MagicWiki
 
+  private
+  def h(text)
+    return CGI.escapeHTML text
+  end
+
+  public
   def view
     magic_wiki_do_setup h(params[:name])
     if params[:commit] && params[:content]
